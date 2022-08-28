e53077e092774ef68d9dcf5567b407fdc8af1242
Esteban Beltrán Valencia <esbv1908@gmail.com>
2022-08-28 18:08:38 -0500

    Agregar archivos de proyecto.

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+using Portafolio.Servicios;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RepositorioProyectos repositorioProyectos;
+        private readonly IServicioEmail servicioEmail;
+
+        public HomeController(ILogger<HomeController> logger, RepositorioProyectos repositorioProyectos, IServicioEmail servicioEmail)
+        {
+            _logger = logger;
+            this.servicioEmail = servicioEmail;
+            this.repositorioProyectos = repositorioProyectos;
+        }
+
+        public IActionResult Index()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyecto().ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+
+            return View(modelo);
+        }
+
+        public IActionResult Proyectos()
+
+        {
+            var proyectos = repositorioProyectos.ObtenerProyecto();
+            return View(proyectos);
+        }
+        [HttpGet]
+        public IActionResult Contacto()
+
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> Contacto(ContactoViewModelcs contactoViewModelcs)
+        {
+            await servicioEmail.Enviar(contactoViewModelcs);
+            return RedirectToAction("Gracias");
+        }
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+
+
+{
+    public class RepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyecto()
+        {
+
+            return new List<Proyecto>() {
+                    new Proyecto
+                    { 
+                    Titulo = "Juego encriptador",
+                    Descripcion = "Puedes encriptar y desencriptar mensajes!",
+                    ImagenUrl = "/imagenes/Muneco.jpg",
+                    Link = "https://estebanbv0.github.io/Encriptador/"
+
+                },
+                    new Proyecto
+                    {
+                    Titulo = "Barberia",
+                    Descripcion = "Visita nuestra barberia y conoce nuestros fabulosos servicios!",
+                    ImagenUrl = "/imagenes/barbar.jpg",
+                    Link = "https://estebanbv0.github.io/Encriptador/"
+                    },
+                    new Proyecto
+                    {
+                    Titulo = "Hospitalización en casa",
+                    Descripcion = "Visita nuestra barberia y conoce nuestros fabulosos servicios!",
+                    ImagenUrl = "/imagenes/diferenciales.jpg",
+                    Link = "https://estebanbv0.github.io/Encriptador/"
+                    }
+            };
+        }
+
+    }
+}
+    
+
